4fe25e1ec2ede3263c50f19b0d3587484d05e1f5
iiihosseiniii <hosseinmousavii2002@gmail.com>
2023-12-03 22:36:53 +0330

    Finish navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,12 @@
     <v-navigation-drawer
       v-model="drawer"
       color="primary"
+      class="rounded-tl-xl rounded-bl-xl"
       right
       absolute
       fixed
       height="100%"
+      :mini-variant.sync="mini"
       :app="!$vuetify.breakpoint.smAndDown"
     >
       <v-icon
@@ -14,6 +16,7 @@
         color="white"
         class="rounded-lg pa-1 float-left mt-5"
         style="background-color: #3f3f40; cursor: pointer"
+        @click="drawer = !drawer"
         >mdi-arrow-right</v-icon
       >
       <v-list-item>
@@ -29,8 +32,9 @@
         <v-list-item
           v-for="(item, index) in items"
           :key="index"
-          class="my-3"
+          class="my-2 py-1"
           link
+          :to="item.link"
         >
           <!--Single List-->
           <div v-if="!item.submenue" class="d-flex align-center">
@@ -49,11 +53,15 @@
               <template #activator>
                 <v-icon class="mr-4" color="gray500">mdi-forum</v-icon>
                 <v-list-item-content class="mr-5">
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  <v-list-item-title class="gray900--text">{{
+                    item.title
+                  }}</v-list-item-title>
                 </v-list-item-content>
               </template>
               <v-list-item v-for="(sub, i) in item.submenue" :key="i" link>
-                <v-list-item-title>{{ sub.title }}</v-list-item-title>
+                <v-list-item-title class="gray900--text">{{
+                  sub.title
+                }}</v-list-item-title>
                 <v-list-item-icon>
                   <v-icon>{{ sub.icon }}</v-icon>
                 </v-list-item-icon>
@@ -64,6 +72,22 @@
       </v-list>
     </v-navigation-drawer>
     <v-main>
+      <v-row class="mt-2">
+        <v-col cols="1">
+          <v-icon
+            v-if="!drawer"
+            size="16"
+            color="white"
+            class="rounded-lg pa-1"
+            style="background-color: #3f3f40; cursor: pointer"
+            @click="drawer = !drawer"
+            >mdi-arrow-left</v-icon
+          >
+        </v-col>
+        <v-col cols="6" md="3">
+          <h2 class="text-right">داشبورد</h2>
+        </v-col>
+      </v-row>
       <v-container>
         <Nuxt />
       </v-container>
@@ -79,7 +103,7 @@ export default {
     return {
       drawer: true,
       items: [
-        { title: 'خلاصه', icon: 'mdi-view-dashboard' },
+        { title: 'خلاصه', icon: 'mdi-view-dashboard', link: '/' },
         { title: 'صفحات', icon: 'mdi-forum' },
         {
           title: 'فروش',
@@ -95,8 +119,21 @@ export default {
             },
           ],
         },
-        { title: 'پیام ها', icon: 'mdi-forum' },
-        { title: 'احراز هویت', icon: 'mdi-forum' },
+        { title: 'پیام ها', icon: 'mdi-forum', link: '/test' },
+        {
+          title: 'احراز هویت',
+          icon: 'mdi-forum',
+          submenue: [
+            {
+              title: 'لیست محصولات',
+              icon: '',
+            },
+            {
+              title: 'صورتحساب',
+              icon: '',
+            },
+          ],
+        },
         { title: 'مستندات', icon: 'mdi-forum' },
         { title: 'اجزا', icon: 'mdi-forum' },
         { title: 'کمک', icon: 'mdi-forum' },
